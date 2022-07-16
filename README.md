2aca528ecf2ecd5186e785ec1c645ee085484034
sinhashlok <shlokjp@gmail.com>
2022-07-16 14:52:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Frontend Mentor - Expenses chart component solution
+
+This is my solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### The challenge
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day’s bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device’s screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+![alt text](https://github.com/sinhashlok/Expenses-Chart-Component/blob/main/Image.png)
+
+### Links
+- Live Site URL: https://sinhashlok.github.io/Expenses-Chart-Component/
+
+## My process
+
+### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Javascript
+
+### What I learned
+- I learned how to listen for hover
+```
+item[0].addEventListener("mouseover", func0, false);
+item[0].addEventListener("mouseout", func01, false);
+```
---
+++ index.js
@@ -0,0 +1,83 @@
+var item = document.querySelectorAll(".bar");
+
+item[0].addEventListener("mouseover", func0, false);
+item[0].addEventListener("mouseout", func01, false);
+function func0()
+{
+   document.querySelectorAll(".boxing")[0].style.display = "block";
+}
+
+function func01()
+{  
+    document.querySelectorAll(".boxing")[0].style.display = "none";
+}
+
+
+item[1].addEventListener("mouseover", func1, false);
+item[1].addEventListener("mouseout", func11, false);
+function func1()
+{
+   document.querySelectorAll(".boxing")[1].style.display = "block";
+}
+
+function func11()
+{  
+    document.querySelectorAll(".boxing")[1].style.display = "none";
+}
+
+item[2].addEventListener("mouseover", func2, false);
+item[2].addEventListener("mouseout", func21, false);
+function func2()
+{
+   document.querySelectorAll(".boxing")[2].style.display = "block";
+}
+
+function func21()
+{  
+    document.querySelectorAll(".boxing")[2].style.display = "none";
+}
+
+item[3].addEventListener("mouseover", func3, false);
+item[3].addEventListener("mouseout", func31, false);
+function func3()
+{
+   document.querySelectorAll(".boxing")[3].style.display = "block";
+}
+
+function func31()
+{  
+    document.querySelectorAll(".boxing")[3].style.display = "none";
+}
+item[4].addEventListener("mouseover", func4, false);
+item[4].addEventListener("mouseout", func41, false);
+function func4()
+{
+   document.querySelectorAll(".boxing")[4].style.display = "block";
+}
+
+function func41()
+{  
+    document.querySelectorAll(".boxing")[4].style.display = "none";
+}
+item[5].addEventListener("mouseover", func5, false);
+item[5].addEventListener("mouseout", func51, false);
+function func5()
+{
+   document.querySelectorAll(".boxing")[5].style.display = "block";
+}
+
+function func51()
+{  
+    document.querySelectorAll(".boxing")[5].style.display = "none";
+}
+item[6].addEventListener("mouseover", func6, false);
+item[6].addEventListener("mouseout", func61, false);
+function func6()
+{
+   document.querySelectorAll(".boxing")[6].style.display = "block";
+}
+
+function func61()
+{  
+    document.querySelectorAll(".boxing")[6].style.display = "none";
+}
